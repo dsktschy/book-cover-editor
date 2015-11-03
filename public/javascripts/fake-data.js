f7d490ba558b5fabc77ac3e7c13ad2d7ef299a0d
dai <chamestica@gmail.com>
2015-11-04 00:19:47 +0900

    Publish the fake data

---
+++ public/javascripts/fake-data.js
@@ -0,0 +1,138 @@
+FAKE_DATA = [
+  {
+    "author": {
+      "align": "center",
+      "bgColor": "#ffffff",
+      "color": "#006da1",
+      "fontFamily": "sans-serif",
+      "fontSize": 65,
+      "height": 96,
+      "positionX": 768,
+      "positionY": 493,
+      "vertical": false,
+      "width": 532,
+      "fontWeight": 400
+    },
+    "band": {
+      "color": "#ffffff",
+      "height": 1448
+    },
+    "bandText": {
+      "align": "center",
+      "bgColor": "transparent",
+      "color": "#4c4948",
+      "fontFamily": "sans-serif",
+      "fontSize": 60,
+      "height": 238,
+      "positionX": 768,
+      "positionY": 1528,
+      "vertical": false,
+      "width": 1292,
+      "fontWeight": 400
+    },
+    "frameImage": {
+      "height": 2048,
+      "positionX": 0,
+      "positionY": 0,
+      "src": "../images/covers/001/original/antique_2B_frame.png",
+      "width": 1536
+    },
+    "logo": {
+      "height": 41,
+      "positionX": 768,
+      "positionY": 1956,
+      "src": "../images/covers/logos/logo_hr_b.gif",
+      "type": "logo_hr_b",
+      "width": 200
+    },
+    "name": 1,
+    "userImage": {
+      "height": 1337,
+      "positionX": 100,
+      "positionY": 552,
+      "src": "../images/covers/001/original/antique_2B_temp.jpg",
+      "width": 1336
+    },
+    "title": {
+      "align": "center",
+      "bgColor": "#ffffff",
+      "color": "#4c4948",
+      "fontFamily": "serif",
+      "fontSize": 115,
+      "height": 325,
+      "positionX": 768,
+      "positionY": 83,
+      "vertical": false,
+      "width": 1243,
+      "fontWeight": 700
+    }
+  },
+  {
+    "author": {
+      "align": "right",
+      "bgColor": "transparent",
+      "color": "#221815",
+      "fontFamily": "sans-serif",
+      "fontSize": 60,
+      "height": 1030,
+      "positionX": 1142,
+      "positionY": 159,
+      "vertical": true,
+      "width": 86,
+      "fontWeight": 600
+    },
+    "band": {
+      "color": "#c6161e",
+      "height": 1498
+    },
+    "bandText": {
+      "align": "left",
+      "bgColor": "transparent",
+      "color": "#ffffff",
+      "fontFamily": "sans-serif",
+      "fontSize": 50,
+      "height": 298,
+      "positionX": 172,
+      "positionY": 1595,
+      "vertical": false,
+      "width": 884,
+      "fontWeight": 500
+    },
+    "frameImage": {
+      "height": 2048,
+      "positionX": 0,
+      "positionY": 0,
+      "src": "../images/covers/002/original/simple_5A_frame.png",
+      "width": 1536
+    },
+    "logo": {
+      "height": 97,
+      "positionX": 1366,
+      "positionY": 1881,
+      "src": "../images/covers/logos/logo_vr_w.gif",
+      "type": "logo_vr_w",
+      "width": 100
+    },
+    "name": 2,
+    "userImage": {
+      "height": 2048,
+      "positionX": 0,
+      "positionY": 0,
+      "src": "../images/covers/002/original/simple_5A_temp.jpg",
+      "width": 948
+    },
+    "title": {
+      "align": "right",
+      "bgColor": "transparent",
+      "color": "#221815",
+      "fontFamily": "sans-serif",
+      "fontSize": 110,
+      "height": 1358,
+      "positionX": 1436,
+      "positionY": 154,
+      "vertical": true,
+      "width": 284,
+      "fontWeight": 600
+    }
+  }
+];
