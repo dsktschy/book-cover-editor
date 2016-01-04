ce6d49936eafa419eb290abd07bf183d239e81b9
dai <chamestica@gmail.com>
2016-01-04 19:17:15 +0900

    Read the fake data beforehand

--- src/javascripts/bce-data-fake.es6.js
+++ src/javascripts/bce-data-fake.es6.js
@@ -8,10 +8,11 @@ const
   /** 本来のデータ取得処理を模倣するための応答遅延時間(msec) */
   DELAY = 1000;
 
-var init, getParsedData;
+var init, getParsedData, isAJAXSimulating;
 
 /**
  * データ取得とパースを模倣する
+ *   script要素が予め存在していなかった場合はAJAXを模倣する
  * @exports
  * @param {string} url
  * @param {Function} onSuccess
@@ -21,17 +22,24 @@ getParsedData = (url, onSuccess, onError) => {
   var splitedURL, num;
   splitedURL = url.split('/');
   num = splitedURL[splitedURL.length - 2];
-  setTimeout(onSuccess.bind(null, FAKE_DATA[num - 1]), DELAY);
+  if (isAJAXSimulating) {
+    setTimeout(onSuccess.bind(null, FAKE_DATA[num - 1]), DELAY);
+    return;
+  }
+  onSuccess(FAKE_DATA[num - 1]);
 };
 
 /**
  * module起動
- *   grobalに変数FAKE_DATAを生成するscriptタグを追加
+ *   grobal変数FAKE_DATAが存在しない場合はそれを定義するscript要素を追加
  *   ブラウザから非推奨の注意文言が出るがデバッグ時限定のためよしとする
  * @exports
  */
 init = () => {
-  $('head').append(HTML);
+  isAJAXSimulating = typeof FAKE_DATA === 'undefined';
+  if (isAJAXSimulating) {
+    $('head').append(HTML);
+  }
 };
 
 export default {
